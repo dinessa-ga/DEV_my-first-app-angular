47e2bc5568b99c34382fc8c83e91117cb1708365
dinessa-ga <dinessa.ga@gmail.com>
2023-09-17 23:12:16 -0500

    tools: instalación de boostrap, popper.js, y add 2 componentes por CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
+import { PersonasComponent } from './personas/personas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent,
+    PersonasComponent
   ],
   imports: [
     BrowserModule
